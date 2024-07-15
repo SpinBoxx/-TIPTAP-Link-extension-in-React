63d63d12ff398e3d051ff1e9f381b71b34b88e2b
SpinBoxx <quentin.mimault@orange.fr>
2024-07-15 14:41:54 +0200

    CHORE: Export functions from services

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 import { LinkExtensionReact } from "./react-link-extension.js";
+import {deleteContentByRange,getHrefFromLinkMarkInRange,isTextSelected,replaceContent} from "./services/tiptap-editor-service.js"
+
+export {LinkExtensionReact, deleteContentByRange,getHrefFromLinkMarkInRange,isTextSelected,replaceContent} 
 export default LinkExtensionReact;
